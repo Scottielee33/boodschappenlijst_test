9961c3dd48d5933a897798969551e0f117506ec5
Scottie Lee <scottie.c.f.lee@gmail.com>
2023-12-01 12:16:28 +0100

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <BoodschappenLijst msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BoodschappenLijst from './components/BoodschappenLijst.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BoodschappenLijst
   }
 }
 </script>
---
+++ src/components/BoodschappenLijst.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="boodschappenlijst">
+    <li v-for="item in items" :key="item">
+      {{ item }}
+    </li>
+    <input type="text" v-model="newItem">
+    <button @click=addItem>Voeg toe</button>
+    <button @click="sortItems">Sorteer</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BoodschappenLijst',
+  data() {
+    return {
+      items: [],
+      newItem: ''
+    }
+  },
+  methods: {
+    addItem() {
+      const newItems = this.newItem.split(',');
+      for (let item of newItems) {
+        item = item.trim();
+        if (item !== '') {
+          this.items.push(item);
+        }
+      }
+      this.newItem = '';
+    },
+    sortItems() {
+      this.items.sort();
+    }
+  }
+}
+
+</script>
